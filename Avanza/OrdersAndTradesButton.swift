abccddb8323fd8148752aa9d4d0dd03573503b1b
OskarGroth <oskar@cindori.org>
2017-06-30 15:40:54 +0200

    iOS View hierarchy & Screenshot

---
+++ Avanza/OrdersAndTradesButton.swift
@@ -0,0 +1,21 @@
+//
+//  OrdersAndTradesButton.swift
+//  Avanza
+//
+//  Created by Oskar Groth on 2017-06-30.
+//  Copyright © 2017 Oskar Groth. All rights reserved.
+//
+
+import UIKit
+
+class OrdersAndTradesButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
